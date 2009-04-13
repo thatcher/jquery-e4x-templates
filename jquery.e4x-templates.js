0c24a3c482b69217c01c163590a36cb2e57f9d58
thatcher <thatcher@christopher-thatchers-macbook-air.local>
2009-04-13 10:22:37 -0400

    corrected extend pattern so blocks logic is applied at the end

--- jquery.e4x-templates.js
+++ jquery.e4x-templates.js
@@ -3,14 +3,15 @@
 */
 (function($, $$){
  
-    var cache = {};
-	blockMapCache = {};
-	var log;
+    var cache = {},
+		stateMapCache = {},
+		blockMapCache = {},
+		log;
 	
-    $.e4x = function(url, model){
+    $.e4x = function(url, model, is_root){
     	log = log||$.logger("E4X");
         var e4x;
-        log.debug("loading e4x : "+ url);
+        log.debug("loading e4x : %s", url);
 		if(!cache[url]){
 	        $.ajax({
 	            url: "app/views/templates/"+url,
@@ -18,12 +19,19 @@
 	            dataType:"text",
 	            async:false,
 	            success: function(text){
+					log.debug('Loaded E4X \n\n%s\n\n', text);
 	                var base, block, blockMap = {};
-	                log.debug("got e4x template \n"+ text);
-	                cache[url] = e4x_eval(text, blockMap, model, true).toXMLString();     
+					if(is_root){
+						//we need to render out the block hierarchy
+						//and cache the result so we don't waste time on each 
+						//actual render with static block logic
+						cache[url] = e4x_eval(text, blockMap, model, true).toString();
+					}else{
+						cache[url] =  e4x_eval(text, blockMap, model, false).toString();
+					} 
 					blockMapCache[url] = blockMap;
-					log.info("template %s : first pass complete", url).
-						debug("\n\n result of first pass : \n\n%s", cache[url]); 
+					log.info("template %s : first pass complete", url);//.
+					//	debug("\n\n (first pass complete) : \n\n%s", cache[url]); 
 					
 					//causes template to be added to file monitor
 					try{load("app/views/templates/"+url)}catch(e){};           
@@ -31,6 +39,8 @@
 				error:function(xhr, status, e){
 					log.error("Error Loading E4X Template : %s", status).
 						exception(e);
+					
+	                cache[url] = e4x_eval("<e4x><h3>Error Loading: "+url+"</h3></e4x>", blockMap, model, true).toXMLString();  
 				}
 	        });  
 		}
@@ -41,29 +51,25 @@
     var e4x_eval = function(text, blockMap, model, deep){
 		var xml_settings = XML.settings();
         //XML.ignoreWhitespace = false;
-		
-		var extend = $.e4x;
-		
 		// the underscore is the key to our template language
 		var _ = function(fn){
 			return '{' + fn + '}';
 		};
-		
-		for(var prop in model||{}){
-			_[prop]  = model[prop];
+		$.extend(_, model);
+		var m = function(prop){
+			if(model && model[prop]){
+				return model[prop];
+			}
+		};
+		var extend = function(url){
+			return $.e4x(url, model, false);
 		}
-		
         var evaluated;
-		if(text.match(/^\s*<>/)){
-			evaluated = eval("(function(){"+
-	            "return new XMLList("+text+");"+
-	        "})();");	
-		}else{
-			evaluated = eval("(function(){"+
-	            "return new XMLList(<e4x>"+text+"</e4x>);"+
-	        "})();").*[0];	
-		}
+		evaluated = eval("(function(){"+
+            "return new XMLList("+text+");"+
+        "})();");	
 					
+		//all blocks are treated in document order
 		var blocks = evaluated..block,
 			compiled,
 			rendered,
@@ -82,11 +88,13 @@
 				}else{
 					id = block.@id.toString();
 					if(!blockMap[id]){
-						log.debug("found block %s", id);
+						//store the default block
+						log.debug("step 1: found block %s", id);
 						blockMap[id] = block.*.copy();
 						delete block.*;
 					}else{
-						log.debug("found override block %s", id);
+						//replace the default block
+						log.debug("step 1: found override block %s", id);
 						blockMap[id] = block.*.copy();
 						delete block.*;
 						delete block;
@@ -94,30 +102,40 @@
 				}
 			}
 			//now loop through the final remaining blocks
-			log.debug("cascaded with blocks \n\n %s", evaluated.toXMLString());
-			compiled = evaluated.copy();
-			log.debug("compiled \n\n %s", compiled.toXMLString());
-			blocks = compiled..block;
-			delete evaluated;
-			for each(block in blocks){
-				id = block.@id.toString();
-				log.debug("\n\n\n\n REPLACING BLOCK %s \n\n\n\n WITH %s", id, blockMap[id]);
-				if(!blockMap[id]){
-					delete block;
+			//log.debug("cascaded with blocks \n\n %s", evaluated.toXMLString());
+			if(evaluated.elements().length() > 0){
+				compiled = evaluated.*[0].copy();
+				//log.debug("step 2 (should have no reference to e4x elements): \n\n %s", compiled.toXMLString());
+				blocks = compiled..block;
+				delete evaluated;
+				for each(block in blocks){
+					id = block.@id.toString();
+					log.debug('step 2: check block '+id);
+					//log.debug("\n\n\n\n REPLACING BLOCK %s \n\n\n\n WITH %s", id, blockMap[id]);
+					if(!blockMap[id]){
+						log.debug('step 2: deleting block '+id);
+						delete block;
+					}
+					log.debug('step 2: replacing block '+id + ' (length :'+blockMap[id].length()+')');
+					compiled..block.(@id == id)[0] = blockMap[id]||'';
+					//log.debug("after replacement \n\n %s", compiled.toXMLString());
 				}
-				compiled..block.(@id == id)[0] = blockMap[id]||'';
-				log.debug("after replacement \n\n %s", compiled.toXMLString());
-				/*if (parent) {
-					parent = block.parent();
-					delete parent.block;
-					parent.appendChild(blockMap[id]);
-				}*/
+				
+				//finally replace each <e4x> element with it's children
+				for each(var e4x in compiled..e4x){
+					log.debug('replacing <e4x>');
+					e4x = e4x.children();
+				}
+				
+				rendered = compiled.copy();
+				delete compiled;
 			}
-			rendered = compiled.copy();
-			delete compiled;
+			
 		}
 		rendered = rendered||evaluated;
-		log.debug("\n\n\n FINAL :\n\n\n", rendered);
+		if (!deep) {
+			//log.debug("\n\n\n step 3 (FINAL) :\n\n\n", rendered);
+		}
 		XML.setSettings(xml_settings);
 		return rendered;
 		
